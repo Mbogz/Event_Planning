cab20d8754befd99cac60f4b7b2f083621e1011a
MutaiAllan <allankiprop175@gmail.com>
2025-01-08 08:10:39 +0300

    Rendered App

--- event-planning-ui/src/index.js
+++ event-planning-ui/src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import { createClient } from '@supabase/supabase-js';
+// import { SessionContextProvider } from '@supabase/auth-helpers-react';
+
+// const supabase = createClient(
+//   "https://touqisgsfkrbxxwehrtc.supabase.co",
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvdXFpc2dzZmtyYnh4d2VocnRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc2MzM0MzIsImV4cCI6MjAxMzIwOTQzMn0.EBtK1xwBjlhuc_yiRbBnTOWsqeQPNxpUXUYYz3DWO9c"
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <SessionContextProvider supabaseClient={supabase}> */}
+      <App />
+    {/* </SessionContextProvider> */}
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
